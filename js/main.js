3cb6c42fce71630591a994d9f77c864e04abd2e3
Selynna Sun <ssun098@gmail.com>
2016-11-06 17:43:53 -0800

    progress is being made

--- js/main.js
+++ js/main.js
@@ -2,35 +2,6 @@ $('.device-list').css('max-height', $(window).height());
 $('.bag').css('max-height', $(window).height());
 $('.bag').css('min-height', $(window).height());
 
-// $.get("https://www.ifixit.com/api/2.0/suggest/phone?doctypes=device", function(data) {
-//     console.log(data);
-// });
-
-// $.get("https://www.ifixit.com/api/2.0/categories", function(data) {
-//     categoriesJSON = JSON.stringify(data);
-//     var parsedCategories = $.parseJSON(categoriesJSON);
-//     console.log(parsedCategories);
-//     var i = 1;
-//     $.each(parsedCategories, function(key1, value1) {
-//         console.log(key1);
-//         var newLi = document.createElement("li");
-//         var newHref = document.createElement("a");
-//         var newContent = document.createTextNode(" " + key1 + " ");
-//         newHref.href="#box-" + i.toString();
-//         i++;
-//         newHref.appendChild(newContent);
-//         newLi.appendChild(newHref);
-
-//         var tabsGroup = document.getElementById("tabs");
-//         tabsGroup.appendChild(newLi);
-//         console.log(value1);
-//     })
-// });
-
-function getInitialItems() {
-    
-}
-
 var listOfDevices = [];
 var listOfBagDevices = [];
 var itemsJSON;
@@ -39,9 +10,7 @@ $.get("https://www.ifixit.com/api/2.0/wikis/CATEGORY?display=hierarchy", functio
     itemsJSON = JSON.stringify(data.hierarchy);
     parsedJSON = $.parseJSON(itemsJSON);
 
-    // console.log(data.hierarchy);
     $.each(parsedJSON, function(key, value) {
-        // console.log(key);
         var newLi = document.createElement("li");
         newContent = document.createTextNode(key);
         newLi.appendChild(newContent);
@@ -63,21 +32,12 @@ $.get("https://www.ifixit.com/api/2.0/wikis/CATEGORY?display=hierarchy", functio
 
 });
 
-// var newLi = document.createElement("li");
-// newContent = document.createTextNode(key);
-// newLi.appendChild(newContent);
-// newLi.className = "list-of-devices device";
-// newLi.id = key;
-
-// listOfDevices.push(newLi);
-
 function moveElements() {
     var boxId = "";
     var indexDeviceList;
     var indexBagList;
     var i = 0;
     var group = document.getElementById("tabs");
-    var tmp2 = parsedJSON;
     var tmp = parsedJSON;
     var tmpArr = [];
     if (tmpArr.length == 0) {
@@ -87,8 +47,12 @@ function moveElements() {
         $('.back').show();
     }
     $(document).on('click', '.list-of-devices', function() {
+        tmp = parsedJSON;
         console.log("clicked");
-        tmp2 = tmp;
+        $.each(tmpArr, function(index, value) {
+            tmp = tmp[value];
+            console.log(tmp);
+        })
         if (tmp[this.id] == null) {
             var newSubLi = document.createElement("li");
             newContent = document.createTextNode(this.id);
@@ -97,18 +61,11 @@ function moveElements() {
             document.getElementById("bag-tabs").appendChild(newSubLi);
         } else {
             tmp = tmp[this.id];
-            $('.list-of-devices').detach();
+            $('.tabs .list-of-devices').detach();
             tmpArr.push(this.id);
             $('.back').show();
             console.log(tmpArr);
             $.each(tmp, function(key, value) {
-                // if (value == null) {
-                //     var newSubLi = document.createElement("li");
-                //     newContent = document.createTextNode(key);
-                //     newSubLi.appendChild(newContent);
-                //     newSubLi.className = "list-of-devices";
-                //     document.getElementById("bag-tabs").appendChild(newSubLi);
-                // }
                 console.log(key);
                 var newSubLi = document.createElement("li");
                 newContent = document.createTextNode(key);
@@ -120,43 +77,17 @@ function moveElements() {
             
         }
 
-        // boxId = document.getElementById(this.id);
-        // indexDeviceList = listOfDevices.indexOf(boxId);
-        // var elementInArray = listOfDevices[indexDeviceList];
-        // indexBagList = listOfBagDevices.indexOf(boxId);
-        // var elementInBagArray = listOfBagDevices[indexBagList];
-
-        // if ($(boxId).parents('.tabs').length == 1) {
-        //     console.log("true");
-        //     listOfDevices.splice(indexDeviceList, 1);
-        //     listOfBagDevices.push(elementInArray);
-        //     for (i = 0; i < listOfBagDevices.length; i++) {
-        //         document.getElementById("bag-tabs").appendChild(listOfBagDevices[i]);
-        //     }
-        // }
-        // else { 
-        //     listOfBagDevices.splice(indexBagList, 1);
-        //     listOfDevices.push(elementInBagArray);
-        //     for (i = 0; i < listOfBagDevices.length; i++) {
-        //         console.log(listOfBagDevices[i]);
-        //     }
-        //     // document.getElementById("tabs").appendChild(elementInArray);
-        //     $(this).appendTo('.tabs');
-        // }
         
     })
-
 
     $('.back').click(function() {
         var location = parsedJSON; 
         for (var i = 0; i < tmpArr.length - 1; i++) {
             location = location[tmpArr[i]];
-            // console.log(location);
         }
         console.log(location);
-        $('.list-of-devices').remove();
+        $('.tabs .list-of-devices').remove();
         $.each(location, function(key, value) {
-            console.log(key);
             var newSubLi = document.createElement("li");
             newContent = document.createTextNode(key);
             newSubLi.appendChild(newContent);
@@ -189,16 +120,10 @@ $('#device-search').on('keyup', function() {
             }
             if (parsedSuggestions.length == 0) {
                 console.log("num suggestion = 0");
-                $('.list-of-devices').detach();
+                $('.tabs .list-of-devices').detach();
                 for (i = 0; i < listOfBagDevices.length; i++) {
                     document.getElementById("bag-tabs").appendChild(listOfBagDevices[i]);
                 }
-                // var content = document.createTextNode("No results");
-                // var newEle = document.createElement("li");
-                // newEle.appendChild(content);
-                // var group = document.getElementById("tabs");
-                // noResultList.push("content");
-                // group.appendChild(noResultList[0]);
             } else {
                 for (i = 0; i < listOfBagDevices.length; i++) {
                     document.getElementById("bag-tabs").appendChild(listOfBagDevices[i]);
